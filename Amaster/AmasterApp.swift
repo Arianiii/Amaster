2dffdeb7e0dffdda8209fc181142668af402ddd0
Arian <arian@Arians-MacBook-Pro.local>
2026-02-03 22:26:40 +0100

    Install

---
+++ Amaster/AmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmasterApp.swift
+//  Amaster
+//
+//  Created by Arian on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmasterTests/AmasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmasterTests.swift
+//  AmasterTests
+//
+//  Created by Arian on 2/3/26.
+//
+
+import Testing
+@testable import Amaster
+
+struct AmasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
